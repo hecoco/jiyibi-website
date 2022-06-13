fb1f7943c321caf2c9626413fa98ea11d6c7b2cb
hecoco <hechao1630@163.com>
2022-06-13 18:08:42 +0800

    deploy

---
+++ precache-manifest.b7f2f02edfddb6dac4daf9c50bba497c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "881e8cca9e0b8feb1396",
+    "url": "css/app.520a4b27.css"
+  },
+  {
+    "revision": "17dc98a04f8b9023a5c2",
+    "url": "css/chunk-vendors.ecd72bac.css"
+  },
+  {
+    "revision": "d4ee3af8c4302e585636940cc91f41c2",
+    "url": "img/qrcode.png"
+  },
+  {
+    "revision": "c9bed50c928e03e43117f09307d703cf",
+    "url": "index.html"
+  },
+  {
+    "revision": "881e8cca9e0b8feb1396",
+    "url": "js/app.8c3b63c8.js"
+  },
+  {
+    "revision": "17dc98a04f8b9023a5c2",
+    "url": "js/chunk-vendors.67e57eac.js"
+  },
+  {
+    "revision": "1464edf7482fe30676bf71fc3886bc17",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
